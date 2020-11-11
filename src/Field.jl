610d658b19a343a8f0ef02f1b55fb2986aff1145
mkfryatt <mkf29@cam.ac.uk>
2020-11-11 22:05:18 +0000

    complete all field operations (apart from division / inversion)

--- src/Field.jl
+++ src/Field.jl
@@ -1,18 +1,18 @@
-import Base: +, -, *, /, ^, ==, !=, repr, inv
+import Base: +, -, *, /, ^, ==, !=, repr, inv, log2, floor, rand
 
 struct FieldMismatchException <: Exception end
 
 struct Field
     order::Int16 #the order of the field
-    reduction::BigInt #the reduction polynomial - x^m
-    Field(m::Number, f::Number) = Field(convert(Int16, m), convert(BigInt, f))
+    reduction::BigInt #the reduction polynomial
+    Field(m::Number, f::Number) = new(convert(Int16, m), convert(BigInt, f))
 end
 
 struct FieldPoint
     x::BigInt
     field::Field
     FieldPoint(x::Number, m::Number, f::Number) = FieldPoint(convert(BigInt, x), Field(m, f))
-    FieldPoint(x::BigInt, field:Field) = new(x, field)
+    FieldPoint(x::Number, field::Field) = new(convert(BigInt, x), field)
 end
 
 function copy(f::Field)
@@ -24,12 +24,27 @@ function copy(a::FieldPoint)
 end
 
 function repr(f::Field)
-    return "Order: "*repr(f.order)*"\nReduction: "*repr(reduction)
+    return "Order: "*repr(f.order)*"\nReduction: "*repr(f.reduction)
 end
 
 function repr(a::FieldPoint)
-    #TODO
-    return "TODO"
+    res = ""
+    i = 0
+    val = a.x
+    while val > 0
+        if val & BigInt(1) > BigInt(0)
+            if i==0
+                res = "1"
+            elseif res==""
+                res = "x^"*repr(i)
+            else
+                res = "x^"*repr(i)*" + "*res
+            end
+        end
+        i += 1
+        val >>>= BigInt(1)
+    end
+    return res
 end
 
 function ==(a::Field, b::Field)
@@ -50,7 +65,6 @@ end
 
 function +(a::FieldPoint, b::FieldPoint)
     if a.field!=b.field throw(FieldMismatchException) end
-    #TODO check
     return FieldPoint(a.x ⊻ b.x, a.field)
 end
 
@@ -59,14 +73,45 @@ function -(a::FieldPoint, b::FieldPoint)
 end
 
 function reduce(a::FieldPoint)
-    #TODO
-    return copy(a)
+    #k = order(a) - a.field.order
+    #i.e. k is the number of bits of a that need to be reduced
+    k = floor(Int, log2(a.x)) - a.field.order
+
+    #reduction(x) = x^order + t(x)
+    t = a.field.reduction ⊻ (BigInt(1) << a.field.order)
+    #now shift t left, and the loop will slowly shift it back down again
+    t <<= k
+
+    result = a.x
+
+    for i in k:-1:0
+        if a.x & (BigInt(1) << (a.field.order+i)) != BigInt(0)
+            result ⊻= t + (BigInt(1)<<(a.field.order+i))
+        end
+        t >>>= 1
+    end
+
+    return FieldPoint(result, a.field)
 end
 
+#left to right, shift and add
 function *(a::FieldPoint, b::FieldPoint)
     if a.field!=b.field throw(FieldMismatchException) end
-    #TODO
-    return copy(a)
+
+    if a.x & BigInt(1) != BigInt(0)
+        result = b.x
+    else
+        result = BigInt(0)
+    end
+
+    for i in 1:(a.field.order-1)
+        if a.x & (BigInt(1)<<i) != BigInt(0)
+            temp = reduce(FieldPoint(b.x << i, b.field))
+            result += temp.x
+        end
+    end
+
+    return FieldPoint(result, a.field)
 end
 
 function /(a::FieldPoint, b::FieldPoint)
@@ -75,16 +120,38 @@ function /(a::FieldPoint, b::FieldPoint)
     return copy(a)
 end
 
+#add a zero between every digit of the original
 function square(a::FieldPoint)
-    #TODO
-    return copy(a)
+    result = BigInt(0)
+
+    counter = BigInt(1)
+    for i in 0:(a.field.order-1)
+        if a.x & counter != BigInt(0)
+            result += BigInt(1) << (i*2)
+        end
+        counter <<= 1
+    end
+
+    return reduce(FieldPoint(result, a.field))
 end
 
-function ^(a::FieldPoint, b::Number)
-    #TODO
+#square and multiply method
+function ^(a::FieldPoint, b::Int)
+    result = FieldPoint(1, a.field)
+    squaring = a
+
+    while b>0
+        if b & BigInt(1) == BigInt(1)
+            result *= squaring
+        end
+        squaring = square(squaring)
+        b >>>= 1
+    end
+
+    return result
 end
 
 function random(f::Field)
-    #TODO
-    return FieldPoint(0, f)
+    range = BigInt(0):((BigInt(1)<<f.order)-BigInt(1))
+    return FieldPoint(rand(range), f)
 end
